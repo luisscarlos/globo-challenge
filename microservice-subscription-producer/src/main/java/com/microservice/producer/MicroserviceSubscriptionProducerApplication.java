705e27d46a87a4449ab7f499024eb46e7db0a881
Luis Carlos <luisscarlostec@gmail.com>
2022-09-26 21:42:10 -0300

    producer microservice first commit

---
+++ microservice-subscription-producer/src/main/java/com/microservice/producer/MicroserviceSubscriptionProducerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSubscriptionProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSubscriptionProducerApplication.class, args);
+	}
+
+}
